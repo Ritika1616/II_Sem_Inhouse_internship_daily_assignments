d69fef1e9a313bd7cf8048154a35e87571967a7e
Ritika1616 <ry7514716@gmail.com>
2026-07-14 00:02:24 +0530

    Create dashboardverticalcontent.php

---
+++ 25ESKCX097/DAY8-DAY12/dashboardverticalcontent.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="row">
+<div class="col-md-3">
+<a href="updatePassword.php">Update Password</a>
+<br>
+<a href="updateProfile.php">Update Password</a>
+
+ </div>
+<div class="col-md-9">

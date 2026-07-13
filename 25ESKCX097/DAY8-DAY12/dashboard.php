57322a39d724248896e75fa3bab76a124029f408
Ritika1616 <ry7514716@gmail.com>
2026-07-14 00:03:41 +0530

    Create dashboard.php

---
+++ 25ESKCX097/DAY8-DAY12/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+include("header2.php");
+include("dashboardverticalcontent.php");
+?>
+<h2><?php  echo "welcome,".$_SESSION['user_name']."!"; 
+?></h2>
+
+
+<?php
+include("footer.php");
+include("dashboardfooter.php");
+?>
+

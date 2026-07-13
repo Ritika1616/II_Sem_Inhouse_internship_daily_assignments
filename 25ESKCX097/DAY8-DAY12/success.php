a4796cb1a853472008661f4332ecb772deec0f7f
Ritika1616 <ry7514716@gmail.com>
2026-07-13 23:54:44 +0530

    Create success.php

---
+++ 25ESKCX097/DAY8-DAY12/success.php
@@ -0,0 +1,19 @@
+<?php
+include("header.php");
+?>
+<div class="container mt-5 text-center">
+    <div class="alert alert-success">
+        <h3>registration successful.</h3>
+</div>
+<a href="login.php" class="btn btn-primary">go back</a>
+</div>
+<?php
+include("footer.php");
+?>
+
+
+
+
+
+
+
